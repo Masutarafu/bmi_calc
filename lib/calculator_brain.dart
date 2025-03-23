1fc03000775f4295bdf92cbab5cc507e895e8557
Farook Ologundudu <ologundudufob@gmail.com>
2025-03-23 15:23:25 +0100

    Few touches here and there

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -12,7 +12,7 @@ class CalculatorBrain {
   late double _bmi;
 
   String calculateBMI() {
-    _bmi = weight / pow(height / 100, 2);
+    _bmi = weight / pow((height / 100), 2);
     return _bmi.toStringAsFixed(1);
   }
 
@@ -27,10 +27,9 @@ class CalculatorBrain {
   }
 
   String getInterpretation() {
-    double bmi = weight / pow(height / 100, 2);
-    if (bmi >= 25) {
+    if (_bmi >= 25) {
       return 'You have a higher than normal body weight. Try to exercise more.';
-    } else if (bmi > 18.5) {
+    } else if (_bmi > 18.5) {
       return 'You have a normal body weight. Good job!';
     } else {
       return 'You have a lower than normal body weight. You can eat a bit more.';
--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         appBarTheme: AppBarTheme(
           backgroundColor: Color(0xFF0A0E21),
